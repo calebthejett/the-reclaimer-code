9a695f99d6bbdd12e7261340ce65f42c794f3165
Caleb Jett <ccj7c5@mst.edu>
2020-04-01 15:58:28 -0500

    Modularize lcd initialization

--- interface/interface.ino
+++ interface/interface.ino
@@ -1,12 +1,14 @@
 #include <LiquidCrystal.h>
 #include "../common/i2c.h"
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+LiquidCrystal lcd(LCD_RS, LCD_EN, LCD_DB4, LCD_DB5, LCD_DB6, LCD_DB7);
+
 void setup() 
 {
-   lcd.begin(16, 2); //initialize the LCD 
-   lcd.print("INTERFACE BOARD\nINITIALIZING...");
-   lcd.setCursor(0, 1);
+   lcd.begin(16, 2); //initialize the LCD as a 16 row by 2 column display
+   lcd.print("INTERFACE BOARD\nINITIALIZING..."); //print welcome
+   lcd.setCursor(0, 1); //jump to second colum, first row
    lcd.print("I2C CLAIM...");
+   Wire.begin(); //join i2c bus as master
 }
 
 void loop() 
