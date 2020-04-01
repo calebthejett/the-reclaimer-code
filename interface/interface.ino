130f4c000c8ed405046dc7f3d0ff9b67dd16e523
Caleb Jett <ccj7c5@mst.edu>
2020-04-01 14:53:09 -0500

    Add LCD initialization

--- interface/interface.ino
+++ interface/interface.ino
@@ -1,6 +1,12 @@
+#include <LiquidCrystal.h>
+#include "../common/i2c.h"
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 void setup() 
 {
-  
+   lcd.begin(16, 2); //initialize the LCD 
+   lcd.print("INTERFACE BOARD\nINITIALIZING...");
+   lcd.setCursor(0, 1);
+   lcd.print("I2C CLAIM...");
 }
 
 void loop() 
