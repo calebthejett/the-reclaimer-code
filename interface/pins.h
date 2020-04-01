b37085974af4f5881f4b2a42d81f5d6046ec3d79
Caleb Jett <ccj7c5@mst.edu>
2020-04-01 15:57:53 -0500

    Add interface board pin declaration header

---
+++ interface/pins.h
@@ -0,0 +1,21 @@
+#ifndef PINS_H
+#define PINS_H
+
+//LCD pins
+#define LCD_DB4 A0
+#define LCD_DB5 A1
+#define LCD_DB6 A2
+#define LCD_DB7 A3
+#define LCD_EN  11
+#define LCD_RS  12
+
+//rotary encoder pins
+#define ENC_A   2
+#define ENC_B   3
+#define ENC_SW  4
+
+//spare test points
+#define TP2     9
+#define TP3     10
+
+#endif
