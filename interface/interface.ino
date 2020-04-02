91dea49baf530c25ecb7056ec9bd7173f84f475f
Caleb Jett <ccj7c5@mst.edu>
2020-04-02 11:46:46 -0500

    Add fancy hourglass animation

--- interface/interface.ino
+++ interface/interface.ino
@@ -2,6 +2,7 @@
 #include <Wire.h>
 #include "i2c.h"
 #include "pins.h"
+#include "lcd_chars.h"
 
 LiquidCrystal lcd(LCD_RS, LCD_EN, LCD_DB4, LCD_DB5, LCD_DB6, LCD_DB7);
 
@@ -16,6 +17,11 @@ void setup()
  pinMode(LCD_DB6,OUTPUT);
  pinMode(LCD_DB7,OUTPUT);
  lcd.begin(16, 2); //initialize the LCD as a 16 row by 2 column display
+ lcd.createChar(BACKSLASH,BACKSLASH_BYTES);
+ lcd.createChar(HOURGLASS_FULL,HOURGLASS_FULL_BYTES);
+ lcd.createChar(HOURGLASS_MID,HOURGLASS_MID_BYTES);
+ lcd.createChar(HOURGLASS_EMPTY,HOURGLASS_EMPTY_BYTES);
+ lcd.setCursor(0, 0); //jump to second colum, first row
  lcd.print("INTERFACE BOARD"); //print welcome
  lcd.setCursor(0, 1); //jump to second colum, first row
  lcd.print("INITIALIZING..."); //print welcome
@@ -29,17 +35,22 @@ void setup()
  attachInterrupt(digitalPinToInterrupt(ENC_SW),isrButton,FALLING);
  lcd.setCursor(0, 1);
  lcd.print("I2C CLAIM...    ");
- //Wire.begin(); //join i2c bus as master
+ Wire.begin(); //join i2c bus as master
 }
-
+//º
 void loop() 
 {
+  noInterrupts();
+  static uint8_t loading_loop_step = 0;
+  lcd.setCursor(15, 1);
+  lcd.write(byte(HOURGLASS_FULL+min(2,loading_loop_step)));
+  loading_loop_step++;
+  if (loading_loop_step==5)
+    loading_loop_step = 0;
   /*static int32_t encoder_delta_last = 0;
   while(encoder_delta_last == encoder_delta)
   {}*/
-  while(!sw_pushed)
-  {}
-  noInterrupts();
+  /*
   sw_pushed = false;
   //encoder_delta_last = encoder_delta;
   lcd.clear();
@@ -54,9 +65,10 @@ void loop()
   {
     lcd.print((PIND & 1 << i)>>i);
   }*/
-  lcd.print(" ");
+  //lcd.print(" ");
   
   interrupts();
+  delay(250);
 }
 
 void isrEncoder()
