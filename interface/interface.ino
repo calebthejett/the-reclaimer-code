4149afe0fbe39ca443f79169b555fabcc9c2484a
Caleb Jett <ccj7c5@mst.edu>
2020-04-02 13:40:02 -0500

    Add basic rendering subroutine

--- interface/interface.ino
+++ interface/interface.ino
@@ -8,6 +8,8 @@ LiquidCrystal lcd(LCD_RS, LCD_EN, LCD_DB4, LCD_DB5, LCD_DB6, LCD_DB7);
 
 volatile int32_t encoder_delta = 0;
 volatile bool sw_pushed = false;
+volatile bool loading = false;
+volatile bool render_now = true;
 void setup() 
 { 
  pinMode(LCD_RS,OUTPUT);
@@ -16,11 +18,14 @@ void setup()
  pinMode(LCD_DB5,OUTPUT);
  pinMode(LCD_DB6,OUTPUT);
  pinMode(LCD_DB7,OUTPUT);
+ 
  lcd.begin(16, 2); //initialize the LCD as a 16 row by 2 column display
+ 
  lcd.createChar(BACKSLASH,BACKSLASH_BYTES);
  lcd.createChar(HOURGLASS_FULL,HOURGLASS_FULL_BYTES);
  lcd.createChar(HOURGLASS_MID,HOURGLASS_MID_BYTES);
  lcd.createChar(HOURGLASS_EMPTY,HOURGLASS_EMPTY_BYTES);
+ 
  lcd.setCursor(0, 0); //jump to second colum, first row
  lcd.print("INTERFACE BOARD"); //print welcome
  lcd.setCursor(0, 1); //jump to second colum, first row
@@ -33,6 +38,7 @@ void setup()
  pinMode(ENC_SW, INPUT_PULLUP);
  attachInterrupt(digitalPinToInterrupt(ENC_A),isrEncoder,CHANGE);
  attachInterrupt(digitalPinToInterrupt(ENC_SW),isrButton,FALLING);
+ 
  lcd.setCursor(0, 1);
  lcd.print("I2C CLAIM...    ");
  Wire.begin(); //join i2c bus as master
@@ -40,39 +46,42 @@ void setup()
 //º
 void loop() 
 {
-  noInterrupts();
+  render();
+}
+
+void render()
+{
   static uint8_t loading_loop_step = 0;
-  lcd.setCursor(15, 1);
-  lcd.write(byte(HOURGLASS_FULL+min(2,loading_loop_step)));
-  loading_loop_step++;
-  if (loading_loop_step==5)
-    loading_loop_step = 0;
-  /*static int32_t encoder_delta_last = 0;
-  while(encoder_delta_last == encoder_delta)
-  {}*/
-  /*
-  sw_pushed = false;
-  //encoder_delta_last = encoder_delta;
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("ENCODER DELTA:");
-  lcd.setCursor(0, 1);
-  
-  
-  lcd.print(encoder_delta);
-  lcd.print(" ");
-  /*for(int i = 0; i < 8; i++)
+  static long last_time = millis();
+
+  if ((millis()-last_time > 250) && (loading==true))
   {
-    lcd.print((PIND & 1 << i)>>i);
-  }*/
-  //lcd.print(" ");
+    render_now = true;
+    last_time = millis();
+  }
+
+  if(render_now)
+  {
+    noInterrupts();
+    lcd.setCursor(15, 1);
+    if (loading)
+    {
+      lcd.write(byte(HOURGLASS_FULL+min(2,loading_loop_step)));
+      loading_loop_step++;
+      if (loading_loop_step==5)
+      loading_loop_step = 0;
+    }
+    else
+      lcd.print(" ");
+    interrupts();
+    render_now = false;
+  }
   
-  interrupts();
-  delay(250);
 }
 
 void isrEncoder()
 {
+  loading = true;
   if (((ENC_PULSE_PORT & ENC_A_MASK) == 0) == ((ENC_PULSE_PORT & ENC_B_MASK) == 0))
     encoder_delta++;
   else
@@ -81,5 +90,6 @@ void isrEncoder()
 
 void isrButton()
 {
-  sw_pushed=true;
+  sw_pushed = true;
+  loading = true;
 }
