603a8b214fa781bf377778a18756bf4a63982e8f
Caleb Jett <ccj7c5@umsystem.edu>
2020-04-27 16:26:39 -0500

    [wip] add test code

--- mainboard/mainboard.ino
+++ mainboard/mainboard.ino
@@ -1,9 +1,21 @@
+#include "pins.h"
+#include "startup.c"
+
 void setup() 
 {
-  
+  init_gpio();
+  pinMode(A4,OUTPUT);
+  pinMode(A5,OUTPUT);
 }
 
 void loop() 
 {
-  
+  digitalWrite(PIN_STAT1,HIGH);
+  digitalWrite(PIN_STAT0,HIGH);
+  digitalWrite(13,HIGH);
+  digitalWrite(A4,LOW);
+  digitalWrite(A5,LOW);
+  //digitalWrite(PIN_STAT1,!digitalRead(PIN_STAT1));
+  //digitalWrite(PIN_STAT0,!digitalRead(PIN_STAT1));
+  //delay(250);
 }
