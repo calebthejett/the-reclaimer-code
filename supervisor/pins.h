bf3dfebeb6d9164ca1c6b41a2f3aa55e4b7c696c
Caleb Jett <ccj7c5@umsystem.edu>
2020-04-29 09:41:52 -0500

    Add pins definition doc

---
+++ supervisor/pins.h
@@ -0,0 +1,21 @@
+#ifndef PINS_H
+#define PINS_H
+
+
+
+#define PIN_24VEN PB1
+#define OFF_24VEN HIGH
+#define ON_24VEN LOW
+
+#define PIN_120VEN PB3
+#define OFF_120VEN LOW
+#define ON_120VEN HIGH
+
+#define PIN_BUZZER PB4
+#define OFF_BUZZER LOW
+#define ON_BUZZER HIGH
+
+
+
+
+#endif
