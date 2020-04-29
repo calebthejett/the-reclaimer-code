6316e9ce6ae0a7966b1f4356ec0e9cf9ca5fe77a
Caleb Jett <ccj7c5@umsystem.edu>
2020-04-29 09:42:05 -0500

    Add basic i2c handling and estop function

--- supervisor/supervisor.ino
+++ supervisor/supervisor.ino
@@ -1,9 +1,65 @@
+#define I2C_SLAVE_ADDRESS 0x10
+#define MSG_ESTOP 0x00
+#include "pins.h"
+#include <TinyWireS.h>
+
+//#ifndef TWI_RX_BUFFER_SIZE
+//#define TWI_RX_BUFFER_SIZE ( 16 )
+//#endif
+
+volatile uint8_t message = 0xFF;
+volatile uint8_t estop_triggered = false;
 void setup() 
 {
-  
+  pinMode(PIN_24VEN,OUTPUT);
+  digitalWrite(PIN_24VEN,OFF_24VEN);
+  pinMode(PIN_120VEN,OUTPUT);
+  digitalWrite(PIN_24VEN,OFF_120VEN);
+  pinMode(PIN_BUZZER,OUTPUT);
+  digitalWrite(PIN_24VEN,OFF_BUZZER);
+  delay(500);
+  digitalWrite(PIN_BUZZER,ON_BUZZER);
+  delay(100);
+  digitalWrite(PIN_BUZZER,OFF_BUZZER);
+
+  TinyWireS.begin(I2C_SLAVE_ADDRESS);
+  TinyWireS.onReceive(receiveEvent);
+  estop();
 }
 
 void loop() 
 {
-  
+    TinyWireS_stop_check();
+}
+void estop()
+{
+  digitalWrite(PIN_24VEN,OFF_24VEN);
+  digitalWrite(PIN_120VEN,OFF_120VEN);
+  digitalWrite(PIN_BUZZER,ON_BUZZER);
+  delay(1000);
+  digitalWrite(PIN_BUZZER,OFF_BUZZER);
+  while(true)
+  {
+    digitalWrite(PIN_BUZZER,ON_BUZZER);
+    delay(100);
+    digitalWrite(PIN_BUZZER,OFF_BUZZER);
+    delay(900);
+  }
+}
+
+
+void receiveEvent(uint8_t howMany)
+{
+  if (!estop_triggered)
+  {
+    for(int i = 0; i < howMany; i++)
+    {
+      uint8_t recv = TinyWireS.receive();
+      if (recv == MSG_ESTOP)
+      {
+        estop_triggered = true;
+        estop();
+      }
+    }
+  }
 }
