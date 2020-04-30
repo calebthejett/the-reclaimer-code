5c2fc6dcaf0938d2b93a517f136643298e3d8b8b
Caleb Jett <ccj7c5@umsystem.edu>
2020-04-30 09:46:07 -0500

    [WIP] add triac control

--- mainboard/gpio.c
+++ mainboard/gpio.c
@@ -4,6 +4,7 @@
 #include "pins.h"
 // The thermal probes are a resistance of 100kohm at 25 degrees C, with a B value of 3950.
 
+
 float get_tz1()
 {
   return ((293.15*3950)/(293.15*log((1024/digitalRead(PIN_TZ1)-1)/100)+3950))-273.15;
@@ -24,6 +25,28 @@ float get_tz4()
   return ((293.15*3950)/(293.15*log((1024/digitalRead(PIN_TZ4)-1)/100)+3950))-273.15;
 }
 
+
+#define MICROS_60HZ 16667
+
+class Triac
+{
+  private:
+    float trigger = MICROS_60HZ;
+    uint8_t pin = 0;
+  public: 
+    Heater (uint8_t pin_val)
+    {
+      pin = pin_val;
+      pinMode(pin, OUTPUT);
+      digitalWrite(pin,LOW);
+    }
+    void set (uint8_t percent) { trigger_phase = asin(percent); }
+    void handle (float last_trigger)
+    {
+      
+    }
+    
+};
 
 
 
