fc49a868190686348a64c9ee67a5a3dee7fbbfba
Caleb Jett <ccj7c5@umsystem.edu>
2020-04-30 13:46:02 -0500

    Validate i2c and temperature probes

--- mainboard/mainboard.ino
+++ mainboard/mainboard.ino
@@ -1,21 +1,43 @@
 #include "pins.h"
-#include "startup.c"
+#include "startup.hpp"
+#include "gpio.hpp"
+#include <Wire.h>
 
 void setup() 
 {
   init_gpio();
-  pinMode(A4,OUTPUT);
-  pinMode(A5,OUTPUT);
+  Wire.begin(); // join i2c bus as master
+  Serial.begin(115200);
+  Serial.setTimeout(50);
+  pinMode(PIN_GRIND,OUTPUT);
 }
 
 void loop() 
 {
-  digitalWrite(PIN_STAT1,HIGH);
-  digitalWrite(PIN_STAT0,HIGH);
-  digitalWrite(13,HIGH);
-  digitalWrite(A4,LOW);
-  digitalWrite(A5,LOW);
-  //digitalWrite(PIN_STAT1,!digitalRead(PIN_STAT1));
-  //digitalWrite(PIN_STAT0,!digitalRead(PIN_STAT1));
-  //delay(250);
+  digitalWrite(PIN_H1,HIGH);
+  digitalWrite(PIN_GRIND,HIGH);
+  if (Serial.available())
+  {
+    uint8_t msg = Serial.readString().toInt();
+    Wire.beginTransmission(0x10);
+    Wire.write(msg);
+    Wire.endTransmission();
+  }
+  Serial.print(millis());
+  Serial.print('\t');
+  Serial.print(32+9.0*get_tz(PIN_TZ1)/5.0);
+  Serial.print('\t');
+  Serial.print(analogRead(PIN_TZ1));
+  Serial.print('\t');
+  Serial.print(32+9.0*get_tz(PIN_TZ2)/5.0);
+  Serial.print('\t');
+  Serial.print(analogRead(PIN_TZ2));
+  Serial.print('\t');
+  Serial.print(32+9.0*get_tz(PIN_TZ3)/5.0);
+  Serial.print('\t');
+  Serial.print(analogRead(PIN_TZ3));
+  Serial.print('\t');
+  Serial.print(32+9.0*get_tz(PIN_TZ4)/5.0);
+  Serial.print('\t');
+  Serial.println(analogRead(PIN_TZ4));
 }
