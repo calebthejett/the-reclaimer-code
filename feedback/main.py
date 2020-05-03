a8d629467290783de77ae8d619fe73261f661cd2
Caleb Jett <ccj7c5@umsystem.edu>
2020-05-03 10:21:58 -0500

    Add serial capture and threading

--- feedback/main.py
+++ feedback/main.py
@@ -25,16 +25,65 @@ def selectFromList(options, message = "Please select from below:"):
         print("invalid selection!")
         print()
 
-def run(port, start):
-  print("")
+class work:
+  def __init__(self, port, start):
+    self.running = True
+    self.port = port
+    self.start = start
+
+  def run(self):
+    print("Opening {}".format(self.port))
+    data = ""
+    with serial.Serial(self.port, baudrate=115200) as ser:
+      while self.running:
+        val = ser.read()
+        try:
+          data += val.decode()
+        except UnicodeDecodeError:
+          print("{}: ignoring {}".format(self.port,str(val)))
+          pass
+
+    print("dumping {}".format(self.port))
+    dest = self.port.replace("/","_")+".csv"
+    with open(os.path.join("./out/",self.start,dest),'w') as f:
+      f.write(data)
+    print("Done!")
+
+  def stop(self):
+    self.running = False
+
 
 
 
 
 if __name__ == "__main__":
-  #ports = glob.glob('/dev/tty[A-Za-z][A-Za-z]*')
-  #selected = selectFromList(ports, "Please select a port:")
+  ports = glob.glob('/dev/tty[A-Za-z][A-Za-z]*')
+  selected = selectFromList(ports, "Please select a port:")
+
   start = str(round(time.time()))
+  try:
+    os.mkdir("./out")
+  except FileExistsError:
+    pass
   os.mkdir(os.path.join("./out/",start))
+
+  workers = []
+  for sel in selected:
+    newWork = work(ports[sel],start)
+    newThread = threading.Thread(target = newWork.run)
+    newThread.daemon = True
+    newThread.start()
+    workers.append([newWork,newThread])
+  
+  try:
+    while True:
+      time.sleep(1)
+  except KeyboardInterrupt:
+    print("Shutting down")
+    for worker in workers:
+      worker[0].stop()
+      worker[1].join()
+
+
   
      
